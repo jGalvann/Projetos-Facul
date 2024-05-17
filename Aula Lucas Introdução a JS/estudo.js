78d70d33e7f4a952b44e8c4f709b14de0e6e432a
jGalvann <joaogalvan02@gmail.com>
2024-05-16 22:29:37 -0300

    aula
    aula lucas dia 16/05

---
+++ Aula Lucas Introdução a JS/estudo.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
+ 
+/*const notaMedia = 3;
+    if (notaMedia >= 6) {
+        alert("aluno passou")
+    }
+    else if (notaMedia >= 4 && notaMedia < 6 ) {
+        alert ("aluno quase passou")
+    }
+    else {
+        alert ("Aluno Reprovaidis")
+    }
+*/
+
+/*const num = 6
+    if (num == 6) {
+        alert ("é igual")
+    }
+    else {
+        alert ("É diferente")
+    }
+*/
+
+/* const professor = "Lucas";
+    if (professor == "Lucas") {
+        console.log("sim é o lucas meudeus")
+    }
+    else {
+        console.log("nao é o mano lucas")
+    }
+*/
+
+//------------------ / -----
+
+/*function primeiraFuncao(parametro, segundoParametro){
+    return parametro + " " + segundoParametro ;
+}
+ const parametro = "essa é uma funçao com parametro";
+ const segundoParametro = "tome uma concatenaçao juntando o texto"
+
+console.log(primeiraFuncao(parametro, segundoParametro));
+*/
+//------------ / --------------
+/*let primeiroArray = [ 
+    "thiaguinho", "jorge tobias", "joao", "marcos"
+]
+
+console.log(primeiroArray[2]); // LEMBRA QUE CHAMA PELO INDICE NAO PELO NUMERO
+})
+*/
+
+/*let pessoa = {
+    nome: "Maria",
+    idade: "17",
+    profissao: "desempregada"
+
+}
+    console.log(pessoa["idade"]);
+*/
+//-------------- / ---------------
+
+/*const numeros = ["A","B","C","D"]
+    numeros.forEach(elemento => {
+        console.log(elemento)
+    })
+*/
+});
--- Aula Lucas Introdução a JS/script.js
+++ Aula Lucas Introdução a JS/script.js
@@ -1,67 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
- 
-/*const notaMedia = 3;
-    if (notaMedia >= 6) {
-        alert("aluno passou")
+document.addEventListener('DOMContentLoaded', function (){
+    const baseImage = {
+        Todas: ['nome.formato', 'animal.jpg','natureza.jpg'],
+        Natureza: [],
+        Cidades: [],
+        Animais: [],
     }
-    else if (notaMedia >= 4 && notaMedia < 6 ) {
-        alert ("aluno quase passou")
-    }
-    else {
-        alert ("Aluno Reprovaidis")
-    }
-*/
 
-/*const num = 6
-    if (num == 6) {
-        alert ("é igual")
-    }
-    else {
-        alert ("É diferente")
-    }
-*/
-
-/* const professor = "Lucas";
-    if (professor == "Lucas") {
-        console.log("sim é o lucas meudeus")
-    }
-    else {
-        console.log("nao é o mano lucas")
-    }
-*/
-
-//------------------ / -----
-
-/*function primeiraFuncao(parametro, segundoParametro){
-    return parametro + " " + segundoParametro ;
-}
- const parametro = "essa é uma funçao com parametro";
- const segundoParametro = "tome uma concatenaçao juntando o texto"
-
-console.log(primeiraFuncao(parametro, segundoParametro));
-*/
-//------------ / --------------
-/*let primeiroArray = [ 
-    "thiaguinho", "jorge tobias", "joao", "marcos"
-]
-
-console.log(primeiroArray[2]); // LEMBRA QUE CHAMA PELO INDICE NAO PELO NUMERO
-})
-*/
-
-/*let pessoa = {
-    nome: "Maria",
-    idade: "17",
-    profissao: "desempregada"
-
-}
-    console.log(pessoa["idade"]);
-*/
-//-------------- / ---------------
-
-const numeros = ["A","B","C","D"]
-    numeros.forEach(elemento => {
-        console.log(elemento)
+    document.body.addEventListener('click', function(event){ //evento de clique
+        
+        if(event.target.classList.contains('botone')){
+            //acessa a lista de calsse de onde clicou e procura pela classe botone
+            alert(823);
+        }
     })
-
-});
+})
