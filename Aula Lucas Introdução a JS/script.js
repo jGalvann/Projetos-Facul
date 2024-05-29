c457776e7239b064cda3f843b3c34a3719862f60
jGalvann <joaogalvan02@gmail.com>
2024-05-29 20:33:14 -0300

    aula lucas
    29 / 05

--- Aula Lucas Introdução a JS/script.js
+++ Aula Lucas Introdução a JS/script.js
@@ -1,3 +1,8 @@
+/*
+
+
+
+*/
 document.addEventListener('DOMContentLoaded', function (){
     const baseImage = {
         Todas: [ 'animal.jpeg','natureza.webp', 'cidade.jpeg'],
@@ -9,27 +14,37 @@ document.addEventListener('DOMContentLoaded', function (){
     function carregaImagens () {
         // selecionar elementos HTML com base em um seletor //
         const galeria = document.querySelector("#galeria-imagens");
-
+        galeria.innerHTML = " ";
         const imagens = baseImage['Todas'];
-
         imagens.forEach(img => {
             console.log(img);
-            galeria.innerHTML += '<div> <img src="imagens/'+ img + ' "/>  </div>';
+            galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/'+ img + ' "/>  </div>';
         })
-
     }
-        carregaImagens();
+        carregaImagens('Todas');
 
-
+    function ordenaImagens (ordem) {
+        const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
+        
+        imagens.sort((a, b)=> {
+            console.log(a);
+        })
+    }
 
 
 
 
     document.body.addEventListener('click', function(event){ //evento de clique
         
-        if(event.target.classList.contains('botone')){
-            //acessa a lista de calsse de onde clicou e procura pela classe botone
-            alert(823);
+        if(event.target.classList.contains('botone-categoria')){
+            //acessa a lista de classe de onde clicou e procura pela classe botone
+            
         }
+        if (event.target.classList.contains('botone-ordenar')) {
+            const ordem = event.target.dataset.ordem;
+            ordenaImagens(ordem);
+
+        }
+
     })
 })
