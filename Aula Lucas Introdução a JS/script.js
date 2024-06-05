011d4e536c80c95f136f6b6401e8a88f23000f4b
jGalvann <joaogalvan02@gmail.com>
2024-06-05 20:29:58 -0300

    aula lucas
    05/06

--- Aula Lucas Introdução a JS/script.js
+++ Aula Lucas Introdução a JS/script.js
@@ -4,31 +4,42 @@
 
 */
 document.addEventListener('DOMContentLoaded', function (){
-    const baseImage = {
-        Todas: [ 'animal.jpeg','natureza.webp', 'cidade.jpeg'],
-        Natureza: ['natureza.webp'],
-        Cidades: ['cidade.jpeg'],
-        Animais: ['animal.jpeg'],
+    const baseImagens = {
+        Todas: [ 'animal.jpg','cidade.jpg', 'natureza.jpg'],
+        Natureza: ['natureza.jpg'],
+        Cidades: ['cidade.jpg'],
+        Animais: ['animal.jpg'],
     }
 
-    function carregaImagens () {
+    function carregaImagens (categoria) {
         // selecionar elementos HTML com base em um seletor //
         const galeria = document.querySelector("#galeria-imagens");
         galeria.innerHTML = " ";
-        const imagens = baseImage['Todas'];
+        const imagens = baseImagens['Todas'];
         imagens.forEach(img => {
             console.log(img);
-            galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/'+ img + ' "/>  </div>';
+            galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/'+ img + ' " alt="'+ img + '"/>  </div>';
         })
     }
         carregaImagens('Todas');
 
     function ordenaImagens (ordem) {
+        console.log(ordem)
         const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
         
-        imagens.sort((a, b)=> {
-            console.log(a);
+        imagens.sort((a, b) => {
+            const nameA = a.querySelector('img').getAttribute('alt');
+            const nameB = b.querySelector('img').getAttribute('alt');
+
+            // operador ternário - se for true ? se nao : 
+            return ordem == 'asc' ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
         })
+        const galeria = document.getElementById('galeria-imagens');
+        galeria.innerHTML = ''; // esvazia box img
+        imagens.forEach(imagem => {
+            galeria.appendChild(imagem);
+        })
+
     }
 
 
@@ -37,8 +48,9 @@ document.addEventListener('DOMContentLoaded', function (){
     document.body.addEventListener('click', function(event){ //evento de clique
         
         if(event.target.classList.contains('botone-categoria')){
-            //acessa a lista de classe de onde clicou e procura pela classe botone
-            
+            const categoria = event.target.dataset.categoria;
+            carregaImagens(categoria);
+            //acessa a lista de classe de onde clicou e procura pela classe botone   
         }
         if (event.target.classList.contains('botone-ordenar')) {
             const ordem = event.target.dataset.ordem;
